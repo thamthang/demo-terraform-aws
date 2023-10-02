eb7f769585808afd578712d86345fddf7d7fd594
DESKTOP-1BTSJHV\THANGTD <thang>
2023-10-02 15:57:54 +0700

    Create main.tf

---
+++ 03-basics/web-app/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "remote" {
+    organization = "thomas-devops-directive"
+
+    workspaces {
+      name = "demo-terraform-aws"
+    }
+  }
+
+  required_providers {
+    aws ={
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
